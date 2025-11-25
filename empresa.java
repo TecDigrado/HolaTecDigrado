94c1ab6bd9ffbdd790121ab4bbe754c7b0fe4967
pablomtz11 <pmartinezg2400@gmail.com>
2025-11-25 10:20:53 -0600

    Commit modificado

--- empresa.java
+++ empresa.java
@@ -1,5 +1,6 @@
 public class empresa {
     public static void main(String[] args) {
         System.out.println("Hola TecDigrado " );
+        System.out.println("Pablo Martinez Garcia");
     }
 }
