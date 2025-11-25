106867116be18184bddfa0898455c497677bbafb
pablomtz11 <pmartinezg2400@gmail.com>
2025-11-25 10:23:00 -0600

    dwadawd

--- empresa.java
+++ empresa.java
@@ -2,5 +2,6 @@ public class empresa {
     public static void main(String[] args) {
         System.out.println("Hola TecDigrado " );
         System.out.println("Pablo Martinez Garcia");
+        System.out.println("Beltran");
     }
 }
